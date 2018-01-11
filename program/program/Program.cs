19f2ca25d98f968eb7a284e8522a6c64a8b56d6d
john cusey <john_cusey@hotmail.com>
2018-01-10 19:20:35 -0500

    Initial Commit

---
+++ program/program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" *** C# Tutorial ***");
+
+            Console.ReadKey();
+        }
+    }
+}
